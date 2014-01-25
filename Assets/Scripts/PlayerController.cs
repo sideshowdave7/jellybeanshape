f0f7fed92158c7c689881d948b813833a6da7c75
Brendan Wanlass <Brendan Wanlass@localhost>
2014-01-25 11:55:35 -0700

    transforming in

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerController : MonoBehaviour {
+
+	public ShapeType _currentShape;
+
+	// Use this for initialization
+	void Start () {
+		_currentShape = ShapeType.Triangle;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetButton("Horizontal"))
+		{
+			this.rigidbody2D.AddForce(new Vector2(Input.GetAxis("Horizontal") * Globals.Instance.PLAYER_SPEED,0));
+		}
+
+		if(Input.GetButton("Vertical"))
+		{
+			this.rigidbody2D.AddForce(new Vector2(0, Input.GetAxis("Vertical") * Globals.Instance.PLAYER_SPEED));
+		}
+
+		if (Input.GetKey(KeyCode.Mouse0) || Input.GetKey(KeyCode.Mouse1))
+		{
+			Vector2 mosPos = Input.mousePosition;
+			Vector3 worldPos = Camera.main.ScreenToWorldPoint((Vector3)mosPos);
+			Vector3 norm = (worldPos - this.transform.position).normalized;
+			this.rigidbody2D.AddForce(norm * Globals.Instance.PLAYER_SPEED);
+		}
+
+		//Check for other shapes in your vicinity
+		GameObject[] shapes = GameObject.FindGameObjectsWithTag("Shape");
+
+		foreach (GameObject g in shapes)
+		{
+			if (Vector2.Distance(g.transform.position, this.transform.position) <= Globals.Instance.INFLUENCE_RADIUS)
+			{
+				ShapeBehavior s = g.GetComponent<ShapeBehavior>();
+				s.UpdateShape(_currentShape, this.gameObject);
+			}
+		}
+
+	
+	}
+
+
+}
---
+++ Assets/Scripts/ShapeBehavior.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShapeBehavior : MonoBehaviour {
+
+
+	private GameObject _target;
+	public ShapeType _shapeType = ShapeType.Circle;
+	
+
+	private SkinnedMeshRenderer _meshRenderer;
+
+	public float maxDistance;
+
+	// Use this for initialization
+	void Start () 
+	{
+		_meshRenderer = this.GetComponent<SkinnedMeshRenderer>();
+		_meshRenderer.SetBlendShapeWeight(2,100);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (_target == null) { //Do nothing
+
+		} else if(_shapeType == ShapeType.Circle) { //move to target //_target.shapeType
+			Vector3 dir =  _target.transform.position - transform.position;
+			rigidbody2D.AddForce (dir);
+		}
+	}
+
+	public void UpdateShape(ShapeType goalShape, GameObject go)
+	{
+		//Transform to goal shape
+		bool changed = false;
+		if(goalShape != ShapeType.Circle)
+		{
+			float currentGoalWeight = _meshRenderer.GetBlendShapeWeight(ShapeToInt(goalShape));
+			if(currentGoalWeight < 100)
+			{
+				_meshRenderer.SetBlendShapeWeight(ShapeToInt(goalShape), ++currentGoalWeight);
+			}
+			else
+			{
+				changed = true;
+			}
+		}
+		for (int i = 0; i < 4; i++)
+		{
+			if (i != ShapeToInt(goalShape))
+			{
+				float currentWeight = _meshRenderer.GetBlendShapeWeight(i);
+				if(currentWeight > 0)
+				{
+					_meshRenderer.SetBlendShapeWeight(i, --currentWeight);
+				}
+				else if (changed)
+				{
+					_target = go;
+				}
+			}
+		}
+	}
+
+	private int ShapeToInt(ShapeType s)
+	{
+		switch (s)
+		{
+		case ShapeType.Triangle:
+			return 0;
+		case ShapeType.Star:
+			return 1;
+		case ShapeType.Hexagon:
+			return 2;
+		case ShapeType.Square:
+			return 3;
+		}
+		return 0;
+	}
+}
