f3fa8ff86af12cff91808c13d907f0ff9bd75ed0
Brendan Wanlass <Brendan Wanlass@localhost>
2014-01-25 09:50:21 -0700

    Added lots o stuff, player controller, moving player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,7 +9,17 @@ public class PlayerController : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
+		if(Input.GetButton("Horizontal"))
+		{
+			this.rigidbody2D.AddForce(new Vector2(Input.GetAxis("Horizontal") * Globals.Instance.PLAYER_SPEED,0));
+		}
+
+		if(Input.GetButton("Vertical"))
+		{
+			this.rigidbody2D.AddForce(new Vector2(0, Input.GetAxis("Vertical") * Globals.Instance.PLAYER_SPEED));
+		}
 	
 	}
 }
