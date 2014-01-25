597fb249fc4f766025d9a8691d7c7489692a2877
john schwarz <johnschwarzorz@gmail.com>
2014-01-25 12:21:00 -0700

    no message

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetButton("Horizontal"))
+		{
+			this.rigidbody2D.AddForce(new Vector2(Input.GetAxis("Horizontal") * Globals.Instance.PLAYER_SPEED,0));
+		}
+
+		if(Input.GetButton("Vertical"))
+		{
+			this.rigidbody2D.AddForce(new Vector2(0, Input.GetAxis("Vertical") * Globals.Instance.PLAYER_SPEED));
+		}
+
+		if (Input.GetKey(KeyCode.Mouse0) || Input.GetKey(KeyCode.Mouse1))
+		{
+			Vector2 mosPos = Input.mousePosition;
+			Vector3 worldPos = Camera.main.ScreenToWorldPoint((Vector3)mosPos);
+			Vector3 norm = (worldPos - this.transform.position).normalized;
+			this.rigidbody2D.AddForce(norm * Globals.Instance.PLAYER_SPEED);
+		}
+	
+	}
+}
---
+++ Assets/Scripts/SlotBehavior.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SlotBehavior : MonoBehaviour {
+
+	public ShapeType shapeType;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		foreach (var shape in GameObject.FindGameObjectsWithTag("Shape")) {
+			var comp = (ShapeBehavior)shape.gameObject.GetComponent<ShapeBehavior>();
+
+				if (comp != null) {
+				var dist = Vector2.Distance(comp.transform.position,transform.position);
+
+				if(dist < .05f) {
+					comp.rigidbody2D.velocity = Vector2.zero;
+					comp.locked = true;
+					comp.transform.position = transform.position;
+					comp.collider2D.enabled = false;
+				} else if (comp._shapeType == shapeType && dist < Globals.Instance.SLOT_TO_SHAPE_DISTANCE && !comp.locked) {
+					Vector2 dir = comp.transform.position - transform.position;
+					comp.rigidbody2D.AddForce(-dir/(Globals.Instance.SLOT_TO_SHAPE_DISTANCE - dist));
+				}
+
+
+			}
+		}
+	}
+}
