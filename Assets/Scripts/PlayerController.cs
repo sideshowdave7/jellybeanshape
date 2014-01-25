e4f2f191724dc22a8d5d7505e32a990bf9b012ed
David Hurst <sideshowdave7@gmail.com>
2014-01-25 09:57:38 -0700

    add circle remove Library folder

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetButton("Horizontal"))
+		{
+			this.rigidbody2D.AddForce(new Vector2(Input.GetAxis("Horizontal") * Globals.Instance.PLAYER_SPEED,0));
+		}
+
+		if(Input.GetButton("Vertical"))
+		{
+			this.rigidbody2D.AddForce(new Vector2(0, Input.GetAxis("Vertical") * Globals.Instance.PLAYER_SPEED));
+		}
+	
+	}
+}
---
+++ Assets/Scripts/ShapeBehavior.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShapeBehavior : MonoBehaviour {
+
+
+	private GameObject _target;
+	public ShapeType _shapeType = ShapeType.Circle;
+
+
+	public float maxDistance;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (_target == null) { //Do nothing
+
+		} else if(_shapeType == ShapeType.Circle) { //move to target //_target.shapeType
+				Vector3 dir = Vector3.MoveTowards(rigidbody.position,_target.rigidbody.position,maxDistance);
+				rigidbody.AddForce (dir);
+		}
+
+	}
+}
+
+public enum ShapeType {
+
+	Circle,
+	Square,
+	Triangle
+
+}
