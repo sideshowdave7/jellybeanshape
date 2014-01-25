f15b53a207e7d61103642abdab60fac885c267a0
David Hurst <sideshowdave7@gmail.com>
2014-01-25 14:54:43 -0700

    add slot prefabs

---
+++ Assets/Scripts/ShapeBehavior.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShapeBehavior : MonoBehaviour {
+
+
+	private GameObject _target;
+	public ShapeType _shapeType = ShapeType.Circle;
+
+	private SkinnedMeshRenderer _meshRenderer;
+
+	public float maxDistance;
+	public bool locked = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+		_meshRenderer = this.GetComponent<SkinnedMeshRenderer>();
+		_meshRenderer.SetBlendShapeWeight(2,100);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (_target == null) { //Do nothing
+
+		} else if(_shapeType == ShapeType.Circle) { //move to target //_target.shapeType
+			Vector3 dir =  _target.transform.position - transform.position;
+			rigidbody2D.AddForce (dir);
+		}
+
+	}
+}
