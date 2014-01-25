cb6f02699b5b776175f2f727d691f25f6b1d7f8e
David Hurst <sideshowdave7@gmail.com>
2014-01-25 09:54:11 -0700

    Merge

---
+++ Assets/Scripts/Globals.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Globals 
+{
+	private Globals ()
+	{
+		globals_init ();
+	}
+
+	private void globals_init()
+	{
+
+	}
+	
+	private static Globals _instance = new Globals ();
+
+	/// <summary>Singleton instance for the active Globals.</summary>
+	/// <value>Singleton instance for the active Globals.</value>
+	public static Globals Instance 
+	{
+		get {
+			if (_instance == null) {
+				_instance = new Globals ();  
+			}
+			return _instance;
+		}
+	}
+
+	#region PLAYER_VARS
+
+	public float PLAYER_SPEED = 100f;
+
+	#endregion
+
+
+}
