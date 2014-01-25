96b68f25256a4c2b9ac1e1823e0c4e501d5471eb
Alex Johnstone <alexander.h.johnstone@gmail.com>
2014-01-25 10:42:38 -0700

    Added models folder and blend shapes, and changed the shapeBehavior script

---
+++ Assets/Scripts/ShapeBehavior.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShapeBehavior : MonoBehaviour {
+
+
+	private GameObject _target;
+	public ShapeType _shapeType = ShapeType.Circle;
+
+	private SkinnedMeshRenderer _meshRenderer;
+
+	public float maxDistance;
+
+	// Use this for initialization
+	void Start () 
+	{
+		_meshRenderer = this.GetComponent<SkinnedMeshRenderer>();
+		_meshRenderer.SetBlendShapeWeight(2,100);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (_target == null) { //Do nothing
+
+		} else if(_shapeType == ShapeType.Circle) { //move to target //_target.shapeType
+			Vector3 dir =  _target.rigidbody.position - rigidbody.position;
+			rigidbody.AddForce (dir);
+		}
+
+	}
+}
---
+++ Assets/Scripts/SlotBehavior.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SlotBehavior : MonoBehaviour {
+
+	public ShapeType shapeType;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		foreach (var shape in GameObject.FindGameObjectsWithTag("Shape")) {
+
+		}
+
+	}
+}
