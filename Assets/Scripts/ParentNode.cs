0e2442e62e5c09e03149c9cba71b4702a8f7895a
David Hurst <sideshowdave7@gmail.com>
2014-01-25 17:17:29 -0700

    new slots and slots working

---
+++ Assets/Scripts/ParentNode.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParentNode : MonoBehaviour {
+
+	public ShapeType _nodeType;
+	public int _initialChildCount;
+	public GameObject _shape;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < _initialChildCount; i++)
+		{
+			GameObject go = (GameObject)Instantiate(_shape);
+			go.GetComponent<ShapeBehavior>()._shapeType = _nodeType;
+			go.GetComponent<ShapeBehavior>().Setup(_nodeType);
+			go.transform.parent = this.transform;
+			go.GetComponent<ShapeBehavior>()._target = this.gameObject;
+
+
+			go.transform.position = new Vector2(this.transform.position.x + Random.Range(-3,3), this.transform.position.y + Random.Range(-3,3));
+
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void SetTargetForChildren(GameObject t)
+	{
+		for (int i = 0; i < ChildCount(); i++)
+		{
+			if (this.transform.GetChild(i).GetComponent<ShapeBehavior>()._target.tag != "Slot"){
+
+				this.transform.GetChild(i).GetComponent<ShapeBehavior>()._target = t;
+				t.GetComponent<PlayerController>()._followerCount ++;
+				t.GetComponent<PlayerController>()._followers.Add(this.transform.GetChild(i).gameObject);
+				this.transform.GetChild(i).transform.parent = null;
+			}
+		}
+		this.GetComponent<CircleCollider2D>().isTrigger = true;
+	}
+
+	public int ChildCount()
+	{
+		return this.transform.childCount;
+	}
+
+	public void UpdateChildren(ShapeType s, GameObject target)
+	{
+		for (int i = 0; i < ChildCount(); i++)
+		{
+			this.transform.GetChild(i).GetComponent<ShapeBehavior>().UpdateShape(s, target, this);
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerController : MonoBehaviour {
+
+	public ShapeType _currentShape;
+	public int _followerCount = 0;
+	public List<GameObject> _followers;
+
+	// Use this for initialization
+	void Start () {
+
+		_followers = new List<GameObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetButton("Horizontal"))
+		{
+			this.rigidbody2D.AddForce(new Vector2(Input.GetAxis("Horizontal") * Globals.Instance.PLAYER_SPEED,0));
+		}
+
+		if(Input.GetButton("Vertical"))
+		{
+			this.rigidbody2D.AddForce(new Vector2(0, Input.GetAxis("Vertical") * Globals.Instance.PLAYER_SPEED));
+		}
+
+		if (Input.GetKey(KeyCode.Mouse0) || Input.GetKey(KeyCode.Mouse1))
+		{
+			Vector2 mosPos = Input.mousePosition;
+			Vector3 worldPos = Camera.main.ScreenToWorldPoint((Vector3)mosPos);
+			Vector3 norm = (worldPos - this.transform.position).normalized;
+			this.rigidbody2D.AddForce(norm * Globals.Instance.PLAYER_SPEED);
+		}
+
+		//Check for other shapes in your vicinity
+		GameObject[] shapes = GameObject.FindGameObjectsWithTag("Shape");
+		if(_followerCount == 0)
+		{
+			this.GetComponent<ShapeBehavior>().UpdateShape(_currentShape, this.gameObject, null);
+		}
+		foreach (GameObject g in shapes)
+		{
+			if (Vector2.Distance(g.transform.position, this.transform.position) <= Globals.Instance.INFLUENCE_RADIUS && g.GetComponent<ShapeBehavior>()._target != this.gameObject)
+			{
+				ShapeBehavior s = g.GetComponent<ShapeBehavior>();
+				if (s.transform.parent != null)
+				{
+					if(s.transform.parent.GetComponent<ParentNode>().ChildCount() <= _followerCount + 1)
+					{
+						s.transform.parent.GetComponent<ParentNode>().UpdateChildren(_currentShape, this.gameObject);
+					}
+					else
+					{
+						this.GetComponent<ShapeBehavior>().UpdateShape(s._shapeType, this.gameObject, s.transform.parent.GetComponent<ParentNode>());
+						for (int i = 0; i < _followerCount; i++)
+						{
+							_followers[i].GetComponent<ShapeBehavior>().UpdateShape(s._shapeType, this.gameObject,null);
+						}
+					}
+				}
+			}
+		}
+
+	
+	}
+
+
+}
