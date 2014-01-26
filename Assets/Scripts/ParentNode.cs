c19299edcf1b7bafbb727c6f1800087e91074af5
Brendan Wanlass <Brendan Wanlass@localhost>
2014-01-25 17:35:04 -0700

    TEH EPIC MUUUURGE

---
+++ Assets/Scripts/ParentNode.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ParentNode : MonoBehaviour {
+
+	public ShapeType _nodeType;
+	public int _initialChildCount;
+	public GameObject _shape;
+	public bool _beingConverted;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < _initialChildCount; i++)
+		{
+			GameObject go = (GameObject)Instantiate(_shape);
+			go.GetComponent<ShapeBehavior>()._shapeType = _nodeType;
+			go.GetComponent<ShapeBehavior>()._originalParentNode = this.gameObject;
+			go.GetComponent<ShapeBehavior>().Setup(_nodeType);
+			go.transform.parent = this.transform;
+			go.GetComponent<ShapeBehavior>()._target = this.gameObject;
+
+
+			go.transform.position = new Vector2(this.transform.position.x + Random.Range(-3,3), this.transform.position.y + Random.Range(-3,3));
+
+		}
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		if(!_beingConverted)
+		{
+			for (int i = 0; i < ChildCount(); i++)
+			{
+				this.transform.GetChild(i).GetComponent<ShapeBehavior>().UpdateShape(_nodeType, null);
+			}
+		}
+	}
+
+	public void SetTargetForChildren(GameObject t)
+	{
+		for (int i = 0; i < ChildCount(); i++)
+		{
+			this.transform.GetChild(i).GetComponent<ShapeBehavior>()._target = t;
+			t.GetComponent<PlayerController>()._followerCount ++;
+			t.GetComponent<PlayerController>()._followers.Add(this.transform.GetChild(i).gameObject);
+			this.transform.GetChild(i).transform.parent = null;
+		}
+		this.GetComponent<CircleCollider2D>().isTrigger = true;
+	}
+
+	public int ChildCount()
+	{
+		return this.transform.childCount;
+	}
+
+	public List<GameObject> GetChildren()
+	{
+		List<GameObject> children = new List<GameObject>();
+		foreach (Transform t in this.transform)
+		{
+			children.Add(t.gameObject);
+		}
+		return children;
+	}
+
+	public void UpdateChildren(ShapeType s, GameObject target)
+	{
+		for (int i = 0; i < ChildCount(); i++)
+		{
+			this.transform.GetChild(i).GetComponent<ShapeBehavior>().UpdateShape(s, this);
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerController : MonoBehaviour {
+
+	public ShapeType _currentShape;
+	public int _followerCount = 0;
+	public List<GameObject> _followers;
+	public bool _beingConverted = false;
+
+	public float _timeIdle = 0f;
+
+	// Use this for initialization
+	void Start () {
+		_currentShape = ShapeType.Triangle;
+		_followers = new List<GameObject>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		if(Input.GetButton("Horizontal"))
+		{
+			this.rigidbody2D.AddForce(new Vector2(Input.GetAxis("Horizontal") * Globals.Instance.PLAYER_SPEED,0));
+		}
+
+		if(Input.GetButton("Vertical"))
+		{
+			this.rigidbody2D.AddForce(new Vector2(0, Input.GetAxis("Vertical") * Globals.Instance.PLAYER_SPEED));
+		}
+
+		if (Input.GetKey(KeyCode.Mouse0) || Input.GetKey(KeyCode.Mouse1))
+		{
+			Vector2 mosPos = Input.mousePosition;
+			Vector3 worldPos = Camera.main.ScreenToWorldPoint((Vector3)mosPos);
+			Vector3 norm = (worldPos - this.transform.position).normalized;
+			this.rigidbody2D.AddForce(norm * Globals.Instance.PLAYER_SPEED);
+		 	
+		}
+
+		if(this.gameObject.rigidbody2D.velocity.magnitude <= 0.5f && _followerCount > 0)
+		{
+			_timeIdle += Time.deltaTime;
+			if (_timeIdle > 2.25f)
+			{
+				_timeIdle = 0f;
+				this.GetComponent<ShapeBehavior>().UpdateShape(_currentShape, null);
+				foreach(GameObject go in _followers)
+				{
+					ShapeBehavior sh = go.GetComponent<ShapeBehavior>();
+					sh._target = sh._originalParentNode;
+					sh.transform.parent = sh._originalParentNode.transform;
+				}
+			}
+
+		}
+
+		//Check for other shapes in your vicinity
+		GameObject[] nodes = GameObject.FindGameObjectsWithTag("Node");
+		//bool c = this.gameObject.GetComponent<ShapeBehavior>()._beingConverted;
+		if(!_beingConverted)
+		{
+			this.GetComponent<ShapeBehavior>().UpdateShape(_currentShape, null);
+		}
+		_beingConverted = false;
+		foreach (GameObject g in nodes)
+		{
+			if (Vector2.Distance(g.transform.position, this.transform.position) <= Globals.Instance.INFLUENCE_RADIUS)
+			{
+				foreach (GameObject shape in g.GetComponent<ParentNode>().GetChildren())
+				{
+					if(shape.GetComponent<ShapeBehavior>()._target != this.gameObject)
+					{
+						g.GetComponent<ParentNode>()._beingConverted=true;
+						ShapeBehavior s = shape.GetComponent<ShapeBehavior>();
+						if (s.transform.parent != null)
+						{
+							if(s.transform.parent.GetComponent<ParentNode>().ChildCount() <= _followerCount + 1)
+							{
+								s.transform.parent.GetComponent<ParentNode>().UpdateChildren(_currentShape, this.gameObject);
+							}
+							else
+							{
+								this.GetComponent<ShapeBehavior>().UpdateShape(s._shapeType, s.transform.parent.GetComponent<ParentNode>());
+								_beingConverted = true;
+								for (int i = 0; i < _followerCount; i++)
+								{
+									_followers[i].GetComponent<ShapeBehavior>().UpdateShape(s._shapeType, null);
+								}
+							}
+						}
+					}
+				}
+			}
+			else
+			{
+				g.GetComponent<ParentNode>()._beingConverted = false;
+			}
+		}
+
+	
+	}
+
+
+}
