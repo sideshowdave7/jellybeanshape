5a4f80e9c7deacf65cfaca55fefb2a5460a27571
john schwarz <johnschwarzorz@gmail.com>
2014-01-25 18:31:45 -0700

    no message

---
+++ Assets/CollideWall.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollideWall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {	
+
+
+	}
+	void OnCollisionEnter2D(Collision2D Box){
+		if(Box.collider.gameObject.name == "MainCharacter")
+		{
+			AudioManager.Instance.playClip("triangle1");
+
+		}
+
+	}
+}
